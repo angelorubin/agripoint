491f91204d1a5d07a1e3589ed8bc8add160ef293
Angelo Rubin <angelorubin@gmail.com>
2021-05-31 18:18:55 -0300

    Implementando pagina plans

--- plans.js
+++ plans.js
@@ -1,4 +1,11 @@
 (function () {
   console.log("script da pagina planos iniciando.. ");
   // window.location.href = "http://127.0.0.1:5500/pagamento.html";
+
+  const cards = document.querySelectorAll(".plans-main__card--default");
+  cards.forEach((el) =>
+    el.addEventListener("click", (event) => {
+      console.log(event.target.id);
+    })
+  );
 })();
