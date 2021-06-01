88c87d09b3c65d7dcfb3e43192e90c28143a3f9a
angelorubin <angelorubin@gmail.com>
2021-06-01 12:39:42 -0300

    Criando a validação do formulário da página de pagamentos

--- payment.js
+++ payment.js
@@ -1,3 +1,31 @@
 (function () {
-  console.log("script da pagina pagamento iniciando");
+  const plan = localStorage.getItem("selected-plan");
+
+  if (plan == "monthly") {
+    document.querySelector(".pay__plan").innerHTML = "Mensal";
+  } else {
+    document.querySelector(".pay__plan").innerHTML = "Anual";
+  }
+
+  document.querySelector(".pay-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const getValueById = (id) => {
+      const value = document.querySelector(`#${id}`).value;
+      return value;
+    };
+
+    const name = getValueById("name");
+    const email = getValueById("email");
+
+    const data = [{ name, email }];
+
+    /*
+    fetch("")
+      .then((resp) => {
+        return resp;
+      })
+      .catch((error) => console.log(error));
+    */
+  });
 })();
