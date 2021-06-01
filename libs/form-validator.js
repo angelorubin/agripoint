edef3664b51f5918366676249d71870ac5d0744c
Angelo Rubin <angelorubin@gmail.com>
2021-06-01 19:16:12 -0300

    Implementando classe de validação para o formulário

---
+++ libs/form-validator.js
@@ -0,0 +1,44 @@
+class FormValidator {
+  constructor(form, fields) {
+    this.form = form;
+    this.fields = fields;
+  }
+
+  initialize() {
+    console.log(this.form);
+    // this.validateOnEntry();
+    this.validateOnSubmit();
+  }
+
+  validateOnSubmit() {
+    const self = this;
+    this.form.addEventListener("submit", function (event) {
+      event.preventDefault();
+      self.fields.map((field) => {
+        const input = document.querySelector(`#${field}`);
+        self.validateFields(input);
+      });
+    });
+  }
+
+  validateFields(field) {
+    if (field.value.trim() === "") {
+      this.setStatus(
+        field,
+        `${
+          (field.previousElementSibling.classList.remove("pay-confirm__label"),
+          field.previousElementSibling.classList.add(
+            "pay-confirm__label--error"
+          ))
+        }`,
+        "Campo obrigatório"
+      );
+    }
+  }
+
+  validateOnEntry() {}
+
+  setStatus(field, message) {
+    console.log({ field, message });
+  }
+}
