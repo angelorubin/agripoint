2161dbaf1c17ca71d31f0ed1e9bd03430ce587e2
angelorubin <angelorubin@gmail.com>
2021-06-01 12:37:09 -0300

    Adicionando plano escolhido ao localStorage

--- plans.js
+++ plans.js
@@ -2,10 +2,11 @@
   console.log("script da pagina planos iniciando.. ");
   // window.location.href = "http://127.0.0.1:5500/pagamento.html";
 
-  const cards = document.querySelectorAll(".plans-main__card--default");
+  const cards = document.querySelectorAll(".card");
   cards.forEach((el) =>
     el.addEventListener("click", (event) => {
-      console.log(event.target.id);
+      const plan = event.currentTarget.id;
+      localStorage.setItem("selected-plan", plan);
     })
   );
 })();
